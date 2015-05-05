4f50d3bfe2191db55a1d91e6dda9177227df91af
Mikhail Malchevskiy <malchmih@gmail.com>
2015-05-05 13:11:24 +0300

    Add paredit package to my layer

--- spacemacs-user/packages.el
+++ spacemacs-user/packages.el
@@ -16,6 +16,7 @@
     solarized-theme
     cider
     monokai
+    paredit
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
