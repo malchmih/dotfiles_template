5890a4ee594ea77c2761fb4e07b47d3ccef9cb37
malch <malchmih@gmail.com>
2015-05-07 08:56:33 +0300

    Upgrade profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,11 @@
 {:user {:plugins [[lein-ancient "0.6.7"]
                   [lein-exec "0.3.4"]
                   [lein-try "0.4.3"]
-                  [jonase/eastwood "0.1.4"]
+                  [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "1.0.5"]
-                  [com.jakemccrary/lein-test-refresh "0.5.4"]]
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]
+                  [com.jakemccrary/lein-test-refresh "0.9.0"]]
+        :dependencies [[pjstadig/humane-test-output "0.7.0"]
                        [org.clojure/tools.nrepl "0.2.10"]]
         :aliases {"anc" ["do" ["ancient" ":all"] ["ancient" "profiles" ":all"]]}
         :injections [(require 'pjstadig.humane-test-output)
