2cd9692ce683cf419b7c3e38f6294ea5c777aad5
malch <malchmih@gmail.com>
2015-05-16 18:59:25 +0300

    Add code quality plugins

--- profiles.clj
+++ profiles.clj
@@ -4,7 +4,10 @@
                   [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "1.1.0-SNAPSHOT"]
-                  [com.jakemccrary/lein-test-refresh "0.9.0"]]
+                  [com.jakemccrary/lein-test-refresh "0.9.0"]
+                  [lein-kibit "0.1.2"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-bikeshed "0.2.0"]]
         :dependencies [[pjstadig/humane-test-output "0.7.0"]
                        [org.clojure/tools.nrepl "0.2.10"]]
         :aliases {"anc" ["do" ["ancient" ":all"] ["ancient" "profiles" ":all"]]}
