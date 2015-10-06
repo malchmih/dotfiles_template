55d779f430091a9a6b63cd6cbc3badea1f4e117e
Mikhail Malchevskiy <malchmih@gmail.com>
2015-10-06 13:47:00 +0300

    Add keybinding for figwheel cljs repl

--- spacemacs-user/packages.el
+++ spacemacs-user/packages.el
@@ -34,8 +34,19 @@ which require an initialization must be listed explicitly in the list.")
       (defun cider-project-reset ()
         (interactive)
         (cider-interactive-eval "(reloaded/reset)"))
+      (defun cider-figwheel-repl ()
+        (interactive)
+        (save-some-buffers)
+        (with-current-buffer (cider-current-repl-buffer)
+          (goto-char (point-max))
+          (insert "(require 'figwheel-sidecar.repl-api)
+             (figwheel-sidecar.repl-api/cljs-repl)")
+          (cider-repl-return)))
+
       (evil-leader/set-key-for-mode 'clojure-mode
         "mj" 'cider-project-reset)
+      (evil-leader/set-key-for-mode 'clojure-mode
+        "msj" 'cider-figwheel-repl)
       (dolist (m '(cider-repl-mode))
         (evil-leader/set-key-for-mode m
           "mc" 'cider-repl-clear-buffer))
