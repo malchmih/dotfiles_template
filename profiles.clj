186cf9767babb37de113a08af1371a763a9c1d9c
Mikhail Malchevskiy <malchmih@gmail.com>
2015-11-05 09:04:31 +0300

    Add dev alias, improve docstrings

--- profiles.clj
+++ profiles.clj
@@ -13,12 +13,16 @@
                   [quickie "0.4.1"]]
         :dependencies [[pjstadig/humane-test-output "0.7.0"]
                        [org.clojure/tools.nrepl "0.2.12"]]
-        :aliases {"anc" ["do"
-                         ["ancient" ":all"]
-                         ["ancient" "check-profiles" ":all"]]
-                  "anc-up" ["do"
-                            ["ancient" "upgrade" ":interactive" ":all"]
-                            ["ancient" "upgrade-profiles" ":interactive" ":all"]]}
+        :aliases {"anc" ^{:doc "Check all dependencies for updates"}
+                  ["do"
+                   ["ancient" ":all"]
+                   ["ancient" "check-profiles" ":all"]]
+                  "anc-up" ^{:doc "Upgrade all dependencies interactively"}
+                  ["do"
+                   ["ancient" "upgrade" ":interactive" ":all"]
+                   ["ancient" "upgrade-profiles" ":interactive" ":all"]]
+                  "dev" ^{:doc "Run development repl"}
+                  [["repl" ":headless"]]}
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
 
