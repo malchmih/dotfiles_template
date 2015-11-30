bc1d8296683c390c41bed62581e936a9337378b8
Mikhail Malchevskiy <malchmih@gmail.com>
2015-11-30 21:29:00 +0300

    Upgrade dependency

--- profiles.clj
+++ profiles.clj
@@ -8,7 +8,7 @@
                   [lein-cljfmt "0.3.0"]
                   [lein-hiera "0.9.5"]
                   [quickie "0.4.1"]]
-        :dependencies [[pjstadig/humane-test-output "0.7.0"]
+        :dependencies [[pjstadig/humane-test-output "0.7.1"]
                        [org.clojure/tools.nrepl "0.2.12"]]
         :aliases {"anc" ^{:doc "Check all dependencies for updates"}
                   ["do"
