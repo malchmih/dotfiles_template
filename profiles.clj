49afbedff3f8bc1cfc490eebef2d492c6dcfef18
Mikhail Malchevskiy <malchmih@virool.com>
2016-11-08 09:50:14 +0300

    Add one more alias for lein

--- profiles.clj
+++ profiles.clj
@@ -22,6 +22,8 @@
                   "anc-snapshot" ^{:doc "Check all dependencies for updates, allow snapshots"}
                   ["ancient" ":allow-all" ":all" ":check-clojure"]
                   "dev" ^{:doc "Run development repl"}
+                  ["repl" ":headless"]
+                  "dev-clean" ^{:doc "Run fully clean development repl"}
                   ["-U" "do" "clean" ["repl" ":headless"]]}}
  :repl {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]}}
