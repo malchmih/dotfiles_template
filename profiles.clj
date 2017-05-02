959c5b5cef8fbf0e5f6e6747189d634764790155
Mikhail Malchevskiy <malchmih@virool.com>
2017-05-02 10:38:09 +0300

    Fix refactor-nrepl version

--- profiles.clj
+++ profiles.clj
@@ -26,6 +26,4 @@
                   "dev-clean" ^{:doc "Run fully clean development repl"}
                   ["-U" "do" "clean" ["repl" ":headless"]]}}
  :repl {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  ;; [refactor-nrepl "2.3.0-SNAPSHOT"]
-                  [refactor-nrepl "2.3.0"]
-                  ]}}
+                  [refactor-nrepl "2.3.0-SNAPSHOT" :upgrade false]]}}
