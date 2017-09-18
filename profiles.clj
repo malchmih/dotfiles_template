31ce3bf79f86fda2835233b17fe36431932be2f9
Misha Malchevskiy <mikhail.malchevskiy@across-log.com>
2017-09-18 10:40:12 +0300

    Upgrade cider-nrepl

--- profiles.clj
+++ profiles.clj
@@ -28,5 +28,5 @@
                   ["repl" ":headless"]
                   "dev-clean" ^{:doc "Run fully clean development repl"}
                   ["-U" "do" "clean" ["repl" ":headless"]]}}
- :repl {:plugins [[cider/cider-nrepl "0.15.1"]
+ :repl {:plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]
                   [refactor-nrepl "2.4.0-SNAPSHOT"]]}}
