0067876255d26d2162496d0f44dab1aecdc59da3
Misha Malchevskiy <malchmih@gmail.com>
2017-10-30 14:00:46 +0300

    Upgrade lein-exec

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.14"]
-                  [lein-exec "0.3.6"]
+                  [lein-exec "0.3.7"]
                   [lein-try "0.4.3"]
                   [jonase/eastwood "0.2.5"]
                   [com.jakemccrary/lein-test-refresh "0.21.1"]
