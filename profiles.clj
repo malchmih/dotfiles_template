c85b6ee73ad405a3b4281a563181bd00e784af45
Mikhail Malchevskiy <malchmih@gmail.com>
2019-10-09 14:35:18 +0300

    Upgrade cider-nrepl

--- profiles.clj
+++ profiles.clj
@@ -22,5 +22,5 @@
                   ["-U" "do" "clean" ["repl" ":headless"]]
                   "deps-dev" ^{:doc "Run lein deps with repl profile"}
                   ["-U" "with-profile" "repl" "deps"]}}
- :repl {:plugins [[cider/cider-nrepl "0.22.4-SNAPSHOT"]
+ :repl {:plugins [[cider/cider-nrepl "0.22.4"]
                   [refactor-nrepl "2.5.0-SNAPSHOT"]]}}
